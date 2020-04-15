66307b7f5eff9afd805e665f859d87793dba6a43
QuirozMarianoMatias <52494903+QuirozMarianoMatias@users.noreply.github.com>
2020-04-15 00:34:44 -0300

    TP1
    tp1 completo

--- TP1/OperacionesAritmeticas.h
+++ TP1/OperacionesAritmeticas.h
@@ -1,10 +1,3 @@
-/** \brief esta funcion suma dos numeros y me devuelve el resultado
- *
- * \param un numero entero
- * \param un numero entero
- * \return devuelve como resultado un entero
- *
- */
 int suma (int a, int b);
 int resta (int a, int b);
 int multiplicacion (int a, int b);
