19aed0cea02f94dccb44d8d6b19040c3e7e0779e
QuirozMarianoMatias <52494903+QuirozMarianoMatias@users.noreply.github.com>
2020-04-15 00:32:49 -0300

    TP1
    tp 1 completo

--- TP1/OperacionesAritmeticas.h
+++ TP1/OperacionesAritmeticas.h
@@ -1,6 +1,12 @@
-#include <stdio.h>
-int suma(int,int);
-int resta (int,int);
-int multiplicacion (int,int);
-float division (int,int);
-int factorial(int);
+/** \brief esta funcion suma dos numeros y me devuelve el resultado
+ *
+ * \param un numero entero
+ * \param un numero entero
+ * \return devuelve como resultado un entero
+ *
+ */
+int suma (int a, int b);
+int resta (int a, int b);
+int multiplicacion (int a, int b);
+float division (int a, int b);
+int factorial(int a);
--- TP1/main.c
+++ TP1/main.c
@@ -3,82 +3,147 @@
 #include "OperacionesAritmeticas.h"
 
 int main()
-{   int numero1;
-    int numero2;
-    float total;
-    char opcion;
-    do
-    {
-        printf("Ingresar primer numero: \n");
-        scanf("%d",&numero1);
-        printf("Ingresar segundo numero: \n");
-        scanf("%d",&numero2);
-        printf("si desea hacer una suma presione:(a)\n");
-        printf("si desea hacer una resta presione:(b)\n");
-        printf("si desea hacer una division presione:(c)\n");
-        printf("si desea hacer una multiplicacion presione:(d)\n");
-        printf("si desea hacer un factorial presione:(e)\n");
-        printf("si desea salir presione:(s)\n");
-        fflush(stdin);
-        scanf("%c", &opcion);
-        switch(opcion)
+{
+
+   int opcion;
+   int flag1 = 0;
+   int flag2 = 0;
+   int flagresultados;
+   int num1;
+   int num2;
+   int totalSuma;
+   int totalResta;
+   int totalFactorial1;
+   int totalFactorial2;
+   int totalMultiplicacion;
+   float totalDivision;
+        do
         {
-            case 'a':
-                total = suma (numero1,numero2);
-                printf("la suma de: %d + %d es: %.2f\n",numero1,numero2,total);
-                break;
-            case 'b':
-                total = resta (numero1,numero2);
-                printf("la resta de: %d - %d es: %.2f\n",numero1,numero2,total);
-                break;
-            case 'c':
-                if (numero2 != 0)
-                {total = division (numero1,numero2);
-                printf("la division de: %d / %d es: %.2f\n",numero1,numero2,total);
-                }
-                else {
-               printf("no se puede dividir por: 0\n");
-                }
-                break;
-            case 'd':
-                total = multiplicacion(numero1,numero2);
-                printf("la multiplicacion de: %d * %d es: %.2f\n",numero1,numero2,total);
-                break;
-            case 'e':
-                if (numero1 >=0)
-                    {
-                    total = factorial(numero1);
-                    printf("el factorial de: %d es: %.2f \n",numero1,total);
-                    }
 
+
+            printf("\n-----------------------CALCULADORA-----------------------\n");
+
+          if (flag1 == 0)
+          {
+              printf("1- Ingresar 1er operando: \n");
+
+          }
+          else
+          {
+              printf("1- Reingresar 1er operando el actual es: (x = %d)\n",num1);
+          }
+            if (flag2==0)
+          {
+              printf("2- Ingresar 2do operando: \n");
+
+          }
+          else
+          {
+              printf("2- Reingresar 2do operando el actual es:(y = %d)\n",num2);
+          }
+            printf("3- Calcular todas las operaciones: \n");
+            printf("4- Informar resultados: \n");
+            printf("5- salir\n");
+            printf("---------------------------------------------------------\n");
+            printf("\nprisone numeros del (1 al 5) para eliger la opcion a usar: ");
+            scanf("%d", &opcion);
+
+
+                switch(opcion)
+                    {case 1:
+                        printf("\ningrese primer operandor: ");
+                        scanf("%d",&num1);
+                        flag1= 1;
+                        break;
+
+                    case 2:
+                        printf("\ningrese segundo operandor: ");
+                        scanf("%d",&num2);
+                        flag2= 1;
+                        break;
+                    case 3: if (flag1 == 1 && flag2 ==1)
+                    {
+                            printf("\na) Calcular la suma (%d+%d)",num1,num2);
+                            totalSuma = suma(num1,num2);
+                            printf("\nb) Calcular la resta (%d-%d)",num1,num2);
+                            totalResta = resta(num1,num2);
+                            if (num2!=0)
+                            {
+                                printf("\nc) Calcular la division (%d/%d)",num1,num2);
+                                totalDivision = division(num1,num2);
+                            }
+                            else
+                            {
+                                printf("\nno se puede dividir por 0\n");
+                            }
+                            printf("\nd) Calcular la multiplicacion (%d*%d)",num1,num2);
+                            totalMultiplicacion = multiplicacion(num1,num2);
+                            if (num1 >= 0 && num2 >= 0)
+                            {
+                                printf("\ne) Calcular el factorial de (%d!) y de (%d!)\n",num1,num2);
+                                totalFactorial1 = factorial(num1);
+                                totalFactorial2 = factorial(num2);
+                            }
+                            else
+                            {
+                                printf("\nno se puede sacar el operando de un numero negativo\n");
+                            }
+
+                    }
                     else
-                    {printf("no se puede sacar factorial de: %d porque es un numero negativo\n",numero1);
-
-                    }
-
-                    if (numero2 >=0)
                     {
-                    total = factorial(numero2);
-                    printf("el factorial de: %d es: %.2f \n",numero2,total);
-
+                        printf("\nusted no ingreso los dos operandos no se puede realizar los calculos\n");
                     }
+                    flagresultados = 1;
+                        break;
+                    case 4:if (flag1 == 1 && flag2 ==1)
+                    {
+                            if(flagresultados == 1)
+                            {
 
-                else
-                    {printf("no se puede sacar factorial de: %d porque es un numero negativo\n",numero2);
 
+
+                            printf("\na) el resultado de la suma (%d+%d) es : %d",num1,num2,totalSuma);
+
+                            printf("\nb) el resultado de la resta (%d-%d) es : %d",num1,num2,totalResta);
+
+                          if(num2!=0)
+                            {
+                            printf("\nc) el resultado de la division (%d/%d) es : %f",num1,num2,totalDivision);
+                            }
+                        else{
+                            printf("\nno se puede dividir por 0");
+                            }
+
+                            printf("\nd) el resultado de la multiplicacion (%d*%d) es : %d",num1,num2,totalMultiplicacion);
+
+                          if (num1 >= 0 && num2 >=0)
+                            {
+                            printf("\ne) el resultado del factorial de (%d!) es: %d y de (%d!) es :  %d\n",num1,totalFactorial1,num2,totalFactorial2);
+                            }
+                        else
+                            {
+                            printf("\nno se puede sacar factorial de numeros negativos\n");
+                            }
+                            flagresultados = 0;
+                        }
+                        else
+                        {
+                            printf("\nno realizo primero el calculo\n");
+                        }
                     }
-                break;
-            case 's':
-                printf("usted salio!\n");
-                break;
-            default:
-                printf("Opcion incorrecta!\n");
-
-
-        }
+                    else
+                        {
+                            printf("no se puede mostrar ningun resultado porque usted no ingreso operandos");
+                        }
+                        break;
+                    case 5:
+                        printf("\nusted salio\n");
+                    default:
+                        printf("\nsu opcion ingresada es erronia\n");
+                    }
     system("pause");
     system("cls");
-    }
-    while(opcion!='s');
-return 0;
-    }
+    }while(opcion != 5);
+    return 0;
+}
